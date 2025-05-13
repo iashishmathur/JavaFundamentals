6ea2922f04a4f0c5555f93df3ff4b747756e220d
Ashish Mathur <67108170+iashishmathur@users.noreply.github.com>
2025-05-13 15:26:43 +0530

    Create DiscountCheck.java
    Added operator examples (arithmetic, logical, ternary)

---
+++ 02-OperatorsAndExpressions/DiscountCheck.java
@@ -0,0 +1,11 @@
+public class DiscountCheck {
+    public static void main(String[] args) {
+        double amount = 1200;
+        boolean isPremiumUser = true;
+
+        double discount = (amount > 1000 && isPremiumUser) ? 0.20 : 0.10;
+        double finalPrice = amount - (amount * discount);
+
+        System.out.println("Final price after discount: ₹" + finalPrice);
+    }
+}
