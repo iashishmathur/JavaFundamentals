3428bc70e28da8c474dfa1e77cb1b4bd793e221d
Ashish Mathur <67108170+iashishmathur@users.noreply.github.com>
2025-05-13 15:27:48 +0530

    Update OperatorExample.java
    Added operator examples (arithmetic, logical, ternary)

--- 02-OperatorsAndExpressions/OperatorExample.java
+++ 02-OperatorsAndExpressions/OperatorExample.java
@@ -4,15 +4,15 @@ public class OperatorExample {
         int b = 3;
 
         // Arithmetic
-        System.out.println("Addition: " + (a + b));
-        System.out.println("Subtraction: " + (a - b));
-        System.out.println("Multiplication: " + (a * b));
-        System.out.println("Division: " + (a / b));
-        System.out.println("Remainder: " + (a % b));
+        System.out.println("Addition: " + (a + b));         // 13
+        System.out.println("Subtraction: " + (a - b));      // 7
+        System.out.println("Multiplication: " + (a * b));   // 30
+        System.out.println("Division: " + (a / b));         // 3
+        System.out.println("Remainder: " + (a % b));        // 1
 
         // Relational
-        System.out.println("a == b: " + (a == b));
-        System.out.println("a > b: " + (a > b));
-        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));          // false
+        System.out.println("a > b: " + (a > b));            // true
+        System.out.println("a < b: " + (a < b));            // false
     }
 }
