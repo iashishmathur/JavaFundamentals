33554aa3c7bbe187ea2bf465a8ca1934daa753e7
Ashish Mathur <67108170+iashishmathur@users.noreply.github.com>
2025-05-13 15:25:41 +0530

    Update MoreOperators.java
    Added operator examples (arithmetic, logical, ternary)

--- 02-OperatorsAndExpressions/MoreOperators.java
+++ 02-OperatorsAndExpressions/MoreOperators.java
@@ -1,1 +1,18 @@
+public class MoreOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
 
+        // Logical
+        boolean result = (x < y) && (x > 0);
+        System.out.println("Logical AND result: " + result);
+
+        // Assignment
+        x += 3;  // x = x + 3
+        System.out.println("After x += 3, x = " + x);
+
+        // Ternary
+        int max = (x > y) ? x : y;
+        System.out.println("Max value: " + max);
+    }
+}
